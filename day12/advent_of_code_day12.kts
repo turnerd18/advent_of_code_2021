f972331db6a60673d8ef995ba3199a1ff46b6f30
Devin Turner <dturner@reggora.com>
2021-12-12 09:05:47 -0600

    day 12 part 1

--- day12/advent_of_code_day12.kts
+++ day12/advent_of_code_day12.kts
@@ -6,15 +6,14 @@ data class Cave(
     val connections: MutableList<Cave> = mutableListOf<Cave>()
 ) {
     override fun toString(): String = "$name | isSmall=$isSmall | connections=${connections.size}"
-    val isSingle: Boolean get() = connections.any { it.isSmall }
 }
 
 val caveNameMap = mutableMapOf<String, Cave>()
 lateinit var start: Cave
 //val FILE_NAME = "example"
-val FILE_NAME = "example_tiny"
+//val FILE_NAME = "example_tiny"
 //val FILE_NAME = "example_large"
-//val FILE_NAME = "input"
+val FILE_NAME = "input"
 File("./$FILE_NAME.txt")
     .readLines()
     .map { it.split("-").filter { !it.isNullOrEmpty() } }
@@ -31,38 +30,19 @@ File("./$FILE_NAME.txt")
 //data class
 
 
-fun countPaths(
-    cave: Cave,
-    maxSmallVisits: Int,
-    smallCaveVisits: Map<String, Int> = mapOf(),
-    caveChain: List<String> = listOf()
-): Int {
+fun countPaths(cave: Cave, smallCaveVisits: Set<String> = setOf(), caveChain: List<String> = listOf()): Int {
     if (cave.name == "end") {
-        println("$caveChain | $smallCaveVisits")
+//      println("$caveChain | $smallCaveVisits")
         return 1
     }
 
-    val nextSmallVisits = smallCaveVisits.toMutableMap()
-    if (cave.isSmall) {
-        nextSmallVisits[cave.name] = (nextSmallVisits[cave.name] ?: 0) + 1
-    }
+    val nextSmallVisits = if (cave.isSmall) smallCaveVisits.plus(cave.name) else smallCaveVisits
 
     var pathsToEnd = 0
-    for (nextCave in cave.connections) {
-        if (nextCave.isSmall) {
-            val nextSmallCaveVisits = smallCaveVisits[nextCave.name] ?: 0
-            val maxVisits = if (nextCave.isSingle) maxSmallVisits else 1
-            if (nextSmallCaveVisits >= maxVisits) continue
-        }
-        pathsToEnd += countPaths(
-            nextCave,
-            maxSmallVisits,
-            nextSmallVisits,
-            caveChain.plus(cave.name)
-        )
+    for (nextCave in cave.connections.filterNot { it.name in smallCaveVisits }) {
+        pathsToEnd += countPaths(nextCave, nextSmallVisits, caveChain.plus(cave.name))
     }
     return pathsToEnd
 }
 
-println("Part 1: ${countPaths(start, 1, mapOf("start" to 1))}")
-println("Part 2: ${countPaths(start, 2, mapOf("start" to 2))}")
+println(countPaths(start))
